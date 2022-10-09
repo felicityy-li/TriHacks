0ebef85283559c548457496d3150fcc2adc0fce3
felicityy-li <88757077+felicityy-li@users.noreply.github.com>
2022-10-09 15:16:47 -0400

    fix: navBar now working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import DoctorsPage from './pages/DoctorsPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/doctors_page" element={<DoctorsPage />} />
