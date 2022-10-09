b41ce25a3996c1728793b30c4ba3ff5ce6ab82cb
felicityy-li <88757077+felicityy-li@users.noreply.github.com>
2022-10-09 16:05:36 -0400

    homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,45 +1,31 @@
-import React from "react"
+import React from "react";
 // import HeroImg from "../assets/hero.svg"
 // import FeatImg from "../assets/feat.svg"
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import lungsBlueGraphic from "../Images/lungsBlueGraphic.png";
 
 export default function HomePage() {
-
-    return (
-        <div className='md:mx-28 mx-4 text-white py-10'>
-
-            <div className='md:grid md:grid-cols-2 items-center'>
-                <div className=''>
-                    <h1 className='text-3xl md:text-6xl'>Who we are</h1>
-                    <p className='text-xl md:text-2xl py-4 tracking-wider text-justify'>CareHive is a web platform that is designed to help detect pneumonia in chest x-ray images, using
-      machine learning. Because pneumonia is a dangerous respitatory diseases, detection at early stages is very important. In fact, young kids and the elderly are the most vulnerable
-      for this infection, which comes with severe consequences. With this application, patients can ensure that they are properly diagnosed and helps reduce medical error in imaging.</p>
-
-                    <Link to="/Signup">
-                        <button className='bg-secondary py-2 px-8 rounded-md text-xl md:text-2xl'>Sign up</button>
-                    </Link>
-
-                </div>
-                <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
-                    <img src="./assets/hero.svg" alt="img" width="450" height="450" />
-                </div>
-            </div>
-
-            <div className='md:grid md:grid-cols-2 pt-12 items-center'>
-                <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
-                    <img src="./assets/feat.svg" alt="img" width="450" height="450" />
-                </div>
-                <div className=''>
-                    <h1 className='text-5xl'>What else do we have</h1>
-                    <p className='text-xl md:text-2xl py-4 tracking-wider'>CareHive is a web app where you can:
-                    </p>
-                    <ul className="text-2xl">
-                        <li className="list-disc">Upload chest x-ray images</li>
-                        <li className="list-disc">Create an account to store your records</li>
-                        <li className="list-disc">Connect with doctors</li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <div style={{ width: "50%", margin: "50px", marginTop: "60px" }}>
+          <h2>Who Are We?</h2>
+          <p>
+            CareHive is a web application with ML integration that helps
+            determine normal chest x-rays vs pneumonic chest x-rays. We
+            leveraged an open source dataset from Kaggle to train our model. Our
+            application can help ensure that there is proper diagnosis and help
+            reduce medical error in imaging. The application gives users a
+            portal to upload chest x-rays which is then run against a ML model
+            to detect pneumonia in chest x-rays.
+          </p>
         </div>
-    )
+        <img
+          src={lungsBlueGraphic}
+          alt=""
+          style={{ width: "30%", height: "30%", marginTop: '30px' }}
+        />
+      </div>
+    </div>
+  );
 }
